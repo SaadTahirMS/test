fe1427ca8a37612cd84cfd9ceaf8617b3700edb4
Saad Tahir <saad.tahir@mindstormstudios.com>
2018-07-09 12:32:27 +0500

    Change LateUpdate to Update

--- SmoothCameraFollow.cs
+++ SmoothCameraFollow.cs
@@ -9,7 +9,7 @@ public class SmoothCameraFollow : MonoBehaviour {
 	public float smoothSpeed = 0.125f;
 	public Vector3 offset;
 
-	void LateUpdate ()
+	void Update ()
 	{
 		Vector3 desiredPosition = target.position + offset;
 		//Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
